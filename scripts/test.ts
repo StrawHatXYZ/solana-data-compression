8496a996951abd03f2e1b640bd653578fa0bd29b
captain <www.abhijeeeth@gmail.com>
2023-09-04 14:47:24 +0530

    data on solana

---
+++ scripts/test.ts
@@ -0,0 +1,75 @@
+import { Keypair, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
+import { ValidDepthSizePair } from "@solana/spl-account-compression";
+import {
+  MetadataArgs,
+  TokenProgramVersion,
+  TokenStandard,
+} from "@metaplex-foundation/mpl-bubblegum";
+import { CreateMetadataAccountArgsV3 } from "@metaplex-foundation/mpl-token-metadata";
+
+// import custom helpers for demos
+import { loadKeypairFromFile, loadOrGenerateKeypair, numberFormatter } from "@/utils/helpers";
+
+// import custom helpers to mint compressed NFTs
+import { createCollection, createTree, mintCompressedNFT } from "@/utils/compression";
+
+// local import of the connection wrapper, to help with using the ReadApi
+import { WrapperConnection } from "@/ReadApi/WrapperConnection";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// define some reusable balance values for tracking
+let initBalance: number, balance: number;
+
+(async () => {
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  // generate a new Keypair for testing, named `wallet`
+  const testWallet = loadOrGenerateKeypair("testWallet");
+
+  // generate a new keypair for use in this demo (or load it locally from the filesystem when available)
+  const payer = process.env?.LOCAL_PAYER_JSON_ABSPATH
+    ? loadKeypairFromFile(process.env?.LOCAL_PAYER_JSON_ABSPATH)
+    : loadOrGenerateKeypair("payer");
+
+  console.log("Payer address:", payer.publicKey.toBase58());
+  console.log("Test wallet address:", testWallet.publicKey.toBase58());
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  // load the env variables and store the cluster RPC url
+  const CLUSTER_URL = process.env.RPC_URL ?? clusterApiUrl("devnet");
+
+  // create a new rpc connection, using the ReadApi wrapper
+  const connection = new WrapperConnection(CLUSTER_URL, "confirmed");
+
+  // get the payer's starting balance (only used for demonstration purposes)
+  initBalance = await connection.getBalance(payer.publicKey);
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  /*
+    Define our tree size parameters
+  */
+  const maxDepthSizePair: ValidDepthSizePair = {
+    // max=16,384 nodes
+    maxDepth: 14,
+    maxBufferSize: 64,
+  };
+  const canopyDepth = maxDepthSizePair.maxDepth - 5;
+
+  /*
+    Actually allocate the tree on chain
+  */
+
+  // define the address the tree will live at
+  const treeKeypair = Keypair.generate();
+
+  // create and send the transaction to create the tree on chain
+  const tree = await createTree(connection, treeKeypair,payer, maxDepthSizePair, canopyDepth);
+
+  /*
